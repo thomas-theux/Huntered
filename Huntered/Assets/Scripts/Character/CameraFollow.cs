a498c98345e35fcdad94de3a637fc7a8238ce904
Thomas Theux <thomasgr@Thomas-Grikschas.local>
2019-05-22 17:35:43 +0200

    Implemented camera follow

---
+++ Huntered/Assets/Scripts/Character/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Rewired;
+
+public class CameraFollow : MonoBehaviour {
+
+	public int cameraID = 0;
+
+	private Transform target;
+
+	public float smoothSpeed = 3.0f;
+	public Vector3 isoOffset;
+
+
+    private void Awake() {
+        target = transform.parent.transform;
+        transform.parent = null;
+    }
+
+
+	private void FixedUpdate() {
+        Vector3 desiredPos = target.position + isoOffset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        
+        transform.position = smoothedPos;
+
+	}
+}
