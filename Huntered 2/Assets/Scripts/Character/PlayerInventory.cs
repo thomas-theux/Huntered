bc2ba43ebd12e04cfe69a62ea3e3ef99354014a4
Thomas Theux <thomasgr@Thomas-MacBook.local>
2019-07-02 15:33:18 +0200

    ms

--- Huntered 2/Assets/Scripts/Character/PlayerInventory.cs
+++ Huntered 2/Assets/Scripts/Character/PlayerInventory.cs
@@ -6,11 +6,4 @@ public class PlayerInventory : MonoBehaviour {
 
     public List<Hashtable> GhostsInventory = new List<Hashtable>();
 
-
-    private void Update() {
-        if (Input.GetKey("g")) {
-            print(GhostsInventory.Count);
-        }
-    }
-
 }
